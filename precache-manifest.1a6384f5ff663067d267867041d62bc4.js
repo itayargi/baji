69d79e49b78fdbcbfdb561b56acd096a18728da8
itayargi <itayhaza@gmail.com>
2019-12-30 20:53:43 +0200

    Updates

---
+++ precache-manifest.1a6384f5ff663067d267867041d62bc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411c14abda042e6db27a73b4083406d8",
+    "url": "/baji/index.html"
+  },
+  {
+    "revision": "eba9ba21636c8aca50c7",
+    "url": "/baji/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e2df2698c5621f16b0b0",
+    "url": "/baji/static/js/2.d19a9682.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/baji/static/js/2.d19a9682.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eba9ba21636c8aca50c7",
+    "url": "/baji/static/js/main.ddad0c08.chunk.js"
+  },
+  {
+    "revision": "609706193515f083d876",
+    "url": "/baji/static/js/runtime-main.02df6da2.js"
+  }
+]);
